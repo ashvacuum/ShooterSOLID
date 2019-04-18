07bba7f5a48e082260725dbf454c643132c56681
ashvacuum <v-2jooro@outlook.com>
2019-04-18 20:18:01 +0800

    Added New characters and created new movement scripts

---
+++ Assets/Scripts/CharInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharInput : MonoBehaviour
+{
+    public float MoveSide { get; private set; }
+    public float MoveUp { get; private set; }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        MoveUp = Input.GetAxisRaw("Vertical");
+        MoveSide = Input.GetAxisRaw("Horizontal");
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 10f;
+
+    private CharInput input;
+
+    void Awake()
+    {
+        input = GetComponent<CharInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPos = transform.position;        
+        newPos.x += input.MoveSide * moveSpeed * Time.deltaTime;
+
+        newPos.y += input.MoveUp * moveSpeed * Time.deltaTime;
+        transform.position = newPos;
+    }
+}
