0d55ab7fae89ed37a840f8de8fa8e6a331d0324a
ashvacuum <v-2jooro@outlook.com>
2019-04-19 20:36:00 +0800

    Testing System.Threading

---
+++ Assets/Scripts/Threading/ThreadQueuer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System;
+
+public class ThreadQueuer : MonoBehaviour
+{
+    List<Action> functionsToRunInMainThread = new List<Action>();
+
+    private void Start()
+    {
+        Debug.Log("Started");
+        functionsToRunInMainThread = new List<Action>();
+        StartThreadedFunction( () => { SlowFunctionThatDoesAUnityThing(Vector3.up); } );
+        Debug.Log("Done");
+    }
+
+    private void Update() 
+    {
+        while(functionsToRunInMainThread.Count > 0)
+        {
+            Action someFunc = functionsToRunInMainThread[0];
+            functionsToRunInMainThread.RemoveAt(0);
+            someFunc();
+        }
+    }
+
+   
+
+    public void StartThreadedFunction(Action someFunction )
+    {
+        Thread t = new Thread(new ThreadStart(someFunction));
+        t.Start();
+    }
+
+    public void QueueMainThreadFunction(Action someFunction)
+    {
+        functionsToRunInMainThread.Add(someFunction);
+    }
+
+    void SlowFunctionThatDoesAUnityThing(Vector3 foo)
+    {
+        Thread.Sleep(2000);
+
+        Action aFunction = () =>
+        {
+            Debug.Log("I am applying to a unity GameObject");
+            this.transform.position += foo;            
+        };
+        QueueMainThreadFunction(aFunction);
+    }
+}
---
+++ Assets/Scripts/Threading/ThreadTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class ThreadTest : MonoBehaviour
+{
+
+    
+    Thread myThread;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start() :: Started");
+        myThread = new Thread( SlowJob );
+        //runs in a new thread
+        myThread.Start();
+        Debug.Log("Start() :: Done");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(myThread.IsAlive)
+            Debug.Log("SlowJob is Running");
+        
+    }
+
+    void SlowJob()
+    {
+
+        Debug.Log("Starting SlowJob doing 1000 things.");
+        
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        sw.Start();
+
+        for(int i = 0; i < 1000; i++)
+        {
+            Thread.Sleep(2);            
+        }
+
+        sw.Stop();
+
+        Debug.Log("Done Executing after " + sw.ElapsedMilliseconds / 1000 + " seconds");
+
+        
+    }
+}
