e05a95d77d88ce8410358e41f4f92d2d33bcde4e
ashvacuum <v-2jooro@outlook.com>
2019-04-20 21:35:56 +0800

    Added Gizmos to Unit.cs - DrawLine

--- Assets/Scripts/AStar Search/Unit.cs
+++ Assets/Scripts/AStar Search/Unit.cs
@@ -47,8 +47,28 @@ public class Unit : MonoBehaviour
                 currentWaypoint = path[targetIndex];
             }
 
-            transform.position = Vector2.MoveTowards(transform.position, currentWaypoint, speed);
+            transform.position = Vector2.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
             yield return null;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if(path != null)
+        {
+            for(int i = targetIndex; i < path.Length; i++)
+            {
+                Gizmos.color = Color.black;
+                Gizmos.DrawCube(path[i], Vector3.one);
+
+                if(i == targetIndex)
+                {
+                    Gizmos.DrawLine(transform.position, path[i]);
+                } else
+                {
+                    Gizmos.DrawLine(path[i - 1], path[i]);
+                }
+            }
         }
     }
 }
